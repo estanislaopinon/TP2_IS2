56a1ca408f67676981e47a8d3a6b4a24d6b323bd
estanislaopinon <estanislaopinon@gmail.com>
2024-04-02 22:52:57 -0300

    documentado

--- ChatGpt.py
+++ ChatGpt.py
@@ -1,15 +1,18 @@
 import argparse
 from openai import OpenAI
 
-client= OpenAI()
+client= OpenAI()#Inicialización del cliente de OpenAI
 interaction_history=[]# Buffer que almacena las consultas y respuestas
 
-def chat_with_chatgpt(userquery, conversation ):
+def chat_with_chatgpt(userquery, conversation ):#Función para interactuar con el modelo de chat de OpenAI y obtener respuestas
     
+   #userquery: consulta del usuario
+   #conversation: indica si se esta utilizando el modo de conversacion
    context=" ".join(interaction_history) if conversation else ""
 
    usertask=" "
    try:
+      #Realiza una solicitud al modelo de chat de OpenAI para obtener la respuesta
       response = client.chat.completions.create(
          model="gpt-3.5-turbo-0125",
          messages=[
@@ -24,29 +27,33 @@ def chat_with_chatgpt(userquery, conversation ):
          presence_penalty=0
       )
       if conversation:
+         #Agrega la consulta y la respuesta al historial de interacciones
          interaction_history.append(userquery)
          interaction_history.append(response.choices[0].message.content)
-
+      #Devuelve la respuesta generada
       return response.choices[0].message.content
       
    except Exception as e:
+      #En caso de error, devuelve un mensaje de error
       return f"Error en la solicitud: {e}"
 
    
 
-def main():
+def main():#Funcion principal del programa
+   #Configuracion del parser de argumentospara aceptar el argumento "--convers"
    parser= argparse.ArgumentParser(description='ChatGPT - Chat with GPT-3.5 AI model.')
    parser.add_argument('--convers', action='store_true', help='Activar el modo de conversación ')
    args = parser.parse_args()
    
    while True:
+      #Imprime el estado del modo de conversación
       print("Modo de conversación" ,("activado" if args.convers else "desactivado"))
       
       while True:
          try:
             userquery= input("Ingrese la Consulta: ")
-            if userquery.strip()== "":
-               print("Ingrese una contraseña valida")
+            if userquery.strip()== "":#En caso de que el usuario no haya ingresado una consulta, se le pide que ingrese una consulta valida
+               print("Ingrese una consulta valida")
                continue
 
             print("YOU: ", userquery)
@@ -59,11 +66,12 @@ def main():
 
             #Almaceno la respuesta en el buffer
             interaction_history.append(answer)
+            #Se imprime la respuesta
             print("chatGPT: ", answer)
-         except KeyboardInterrupt:
+         except KeyboardInterrupt:# Interrupción del teclado (Ctrl+C) para salir del bucle de conversación
             print("\nSaliendo del programa")
             break
-         except Exception as e:
+         except Exception as e:#Cualquier error que pueda ocurrir durante la interacción con chatGPT, se informa que se produjo un error
             print(f"Se ha producido un error: {e}")
       
 
